d94fb190ef1dd2f6224712807c9890ca928db072
szabosandor0910 <78804107+szabosandor0910@users.noreply.github.com>
2022-05-25 23:59:01 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css%22%3E
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css%22%3E">
+  <link rel="stylesheet" href="style.scss">
+
 </head>
 <body>
-<div class="container">
-  <div>
+<div>
+  <div class="title">
     <h2>Szabó Sándor</h2>
+    <p>Adatbázis (CRUD) kezelő weboldala!</p>
+
   </div>
-  <p>Adatbázis (CRUD) kezelő weboldala!</p>
   <div class="box">
-     <form class="form-inline m-2" action="create.php" method="POST">
+     <form class="form-inline" action="create.php" method="POST">
         <label for="name">Név:</label>
-        <input type="text" class="form-control m-2" id="name" name="name">
-        <label for="score">Pont:</label>
-        <input type="number" class="form-control m-2" id="score" name="score">
-        <button type="submit" class="btn btn-primary">Hozzáad</button>
+        <input type="text" class="form-control" id="name" name="name">
+        <label for="score">Jegy:</label>
+        <input type="number" class="form-control" id="score" name="score">
+        <button type="submit" class="button-12" role="button">Hozzáad</button>
     </form>
+      <div class="container">
     <table class="table">
         <tbody>
         <?php include 'read.php'; ?>
         </tbody>
     </table>
+      </div>
   </div>
 
  
